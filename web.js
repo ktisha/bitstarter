e73fbf0d13092b517541d5400704d6cd4de0c34c
ktisha <tuzova.k@gmail.com>
2013-07-07 16:29:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   output = fs.readFile("index.html");
-  var buffer = new Buffer("I'm a string!", "utf-8");
+  var buffer = new Buffer(output, "utf-8");
   response.send(buffer.toString());
 });
 
