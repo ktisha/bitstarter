d3f6a8ad640f8f0ae0c3fd06d97fd5f48e4e9296
ktisha <tuzova.k@gmail.com>
2013-07-07 16:28:34 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  output = fs.readFile("index.html");
+  var buffer = new Buffer("I'm a string!", "utf-8");
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
